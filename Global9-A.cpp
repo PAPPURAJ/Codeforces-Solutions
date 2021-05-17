061a7bd4f2a960eb317484b0f714b83013d87119
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2021-05-17 21:49:38 +0600

    Create Global9-A.cpp

---
+++ Global9-A.cpp
@@ -0,0 +1,33 @@
+Link: https://codeforces.com/contest/1375/problem/A
+
+#include<bits/stdc++.h>
+#define ff unsigned int ttt;cin>>ttt;for(int zzz=0;zzz<ttt;zzz++)
+#define ll long long int
+#define ui unsigned int
+#define ull unsigned long long int
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl '\n';
+ 
+using namespace std;
+void solve();
+ 
+ 
+ 
+main(){
+    FAST
+     ff  solve();
+    }
+ 
+ 
+ 
+ 
+void solve(){
+ int n,p;
+ cin>>n;
+ for(int i=0;i<n;i++){
+    cin>>p;
+    (i&1)?cout<<-abs(p)<<" ":cout<<abs(p)<<" ";
+ }
+ 
+cout<<nl;
+}
