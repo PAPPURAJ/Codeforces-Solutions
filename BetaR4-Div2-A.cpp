75ebeacd34c0c5ad28cc06c98b08d5bdc26e6c62
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2021-05-21 20:54:36 +0600

    Create BetaR4-Div2-A.cpp

---
+++ BetaR4-Div2-A.cpp
@@ -0,0 +1,16 @@
+A. Watermelon
+Link: https://codeforces.com/contest/4/problem/A
+
+#include<iostream>
+    using namespace std;
+ 
+    int main(){
+ 
+        int w;
+        cin>>w;
+        if(w%2==0 && w>2)
+            cout<<"YES";
+        else
+            cout<<"NO";
+        return 0;
+    }
