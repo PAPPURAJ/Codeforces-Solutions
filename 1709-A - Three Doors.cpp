d20ca1daa148592f71df8e313428b2542cd5b323
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2022-07-26 23:41:34 +0600

    Create 1709-A - Three Doors.cpp

---
+++ 1709-A - Three Doors.cpp
@@ -0,0 +1,45 @@
+//URL: https://codeforces.com/contest/1709/problem/A
+
+
+#include<bits/stdc++.h>
+#define ff unsigned int ttt;cin>>ttt;for(int zzz=0;zzz<ttt;zzz++)
+#define ll long long int
+#define ui unsigned int
+#define ull unsigned long long int
+#define SPEED ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl '\n'
+#define Fi(ii,jj) for(int i=ii;i<jj;i++)
+#define Fj(iii,jjj) for(int j=iii;j<jjj;j++)
+#define strSort(sss) sort(sss.begin(),sss.end())
+#define read freopen ("in.txt","r",stdin)
+
+using namespace std;
+void solve();
+
+
+main(){
+       // read;
+        SPEED  ff solve();
+    }
+
+
+
+
+
+void solve(){
+    int x[4],t=0;
+    cin>>x[0]>>x[1]>>x[2]>>x[3];
+
+    for(int i=0;i<4;i++){
+        t=x[t];
+        if(i!=3 && t==0){
+            cout<<"NO"<<nl;
+            return;
+        }
+
+    }cout<<"YES"<<nl;
+
+
+  }
+
+
