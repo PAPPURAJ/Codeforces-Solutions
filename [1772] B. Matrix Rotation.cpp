731c36049ccb8840d9a8012d7bd37bbda74d5340
PAPPURAJ BHOTTACHARJEE <45383061+PAPPURAJ@users.noreply.github.com>
2022-12-18 22:50:20 +0600

    Create [1772] B. Matrix Rotation.cpp

---
+++ [1772] B. Matrix Rotation.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#define ff unsigned int ttt;cin>>ttt;for(int zzz=0;zzz<ttt;zzz++)
+#define ll long long int
+#define ui unsigned int
+#define ull unsigned long long int
+#define SPEED ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define nl '\n'
+#define Fi(ii,jj) for(int i=ii;i<jj;i++)
+#define Fid(ii,jj) for(int i=jj-1;i>=ii;i--)
+#define Fj(iii,jjj) for(int j=iii;j<jjj;j++)
+#define Fk(iiii,jjjj) for(int k=iiii;k<jjjj;k++)
+#define strSort(sss) sort(sss.begin(),sss.end())
+#define intArSort(arr) sort(arr, arr + sizeof(arr) / sizeof(arr[0]))
+#define TEST freopen ("in.txt","r",stdin);freopen ("out.txt","w",stdout);
+#define py cout<<"YES\n"
+#define pn cout<<"NO\n"
+
+using namespace std;
+void solve();
+ 
+
+int main(){
+        
+       //TEST
+        SPEED  ff solve();
+      
+        return 0;
+    }
+
+
+
+
+
+void solve(){
+
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if((a<b && c<d && a<c && b<d) || (c<a && d<b && c<d && a<b) || (d<c && b<a && b>d && c<a) || (b<d && a<c && b<a && d<c))
+        py;
+    else
+        pn;
+
+
+  }
+
